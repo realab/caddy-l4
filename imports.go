222185f5bf7d59886081439ea7f5c38f980c0bc7
zhoujiahui <zhoujiahui@bilibili.com>
2025-05-09 19:38:32 +0800

    imports

--- imports.go
+++ imports.go
@@ -29,6 +29,7 @@ import (
 	_ "github.com/mholt/caddy-l4/modules/l4rdp"
 	_ "github.com/mholt/caddy-l4/modules/l4regexp"
 	_ "github.com/mholt/caddy-l4/modules/l4remoteiplist"
+	_ "github.com/mholt/caddy-l4/modules/l4shadowtls"
 	_ "github.com/mholt/caddy-l4/modules/l4socks"
 	_ "github.com/mholt/caddy-l4/modules/l4ssh"
 	_ "github.com/mholt/caddy-l4/modules/l4subroute"
